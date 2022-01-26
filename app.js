9c379a060ce5c12f6e8c67bd069ac96ed5fc0404
AB <amarbat@gmail.com>
2022-01-25 21:05:42 -0500

    Add Authentication

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express               = require ('express');
+var path                  = require ('path');
+var cookieParser          = require ('cookie-parser');
+var logger                = require ('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+require ("dotenv").config ();
+require ("./config/database").connect ();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var indexRouter           = require ('./routes/index');
+var usersRouter           = require ('./routes/users');
+var authRouter            = require ('./routes/auth');
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var app                   = express();
 
-module.exports = app;
+app.use (logger ('dev'));
+app.use (express.json ());
+app.use (express.urlencoded ({ extended: false }));
+app.use (cookieParser ());
+app.use (express.static (path.join (__dirname, 'public')));
+
+app.use ('/', indexRouter);
+app.use ('/users', usersRouter);
+app.use ('/auth', authRouter);
+
+module.exports            = app;
