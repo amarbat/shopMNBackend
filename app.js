4826bd535c3d3b15ef355d3010999c0277e4ebe8
AB <amarbat@gmail.com>
2022-01-30 21:47:13 -0500

    Add Authentication 2

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-var express               = require ('express');
-var path                  = require ('path');
-var cookieParser          = require ('cookie-parser');
-var logger                = require ('morgan');
+const express             = require ('express');
+const path                = require ('path');
+const cookieParser        = require ('cookie-parser');
+const logger              = require ('morgan');
+const auth                = require ("./middleware/auth")
 
 
 require ("dotenv").config ();
 require ("./config/database").connect ();
 
-var indexRouter           = require ('./routes/index');
-var usersRouter           = require ('./routes/users');
-var authRouter            = require ('./routes/auth');
+const authRouter          = require ('./routes/auth');
 
 var app                   = express();
 
@@ -19,8 +18,6 @@ app.use (express.urlencoded ({ extended: false }));
 app.use (cookieParser ());
 app.use (express.static (path.join (__dirname, 'public')));
 
-app.use ('/', indexRouter);
-app.use ('/users', usersRouter);
 app.use ('/auth', authRouter);
 
 module.exports            = app;
